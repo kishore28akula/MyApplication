7a206a677324760042c471bfaaf5ffbdf5dea3ea
kishore28akula <kishore28akula@gmail.com>
2017-05-22 11:58:38 +0530

    Update DrivingGraph.java

--- app/src/main/java/com/testing/venkatkishore/drivinginfo/DrivingGraph.java
+++ app/src/main/java/com/testing/venkatkishore/drivinginfo/DrivingGraph.java
@@ -75,9 +75,8 @@ public class DrivingGraph extends AppCompatActivity implements View.OnClickListe
     Uri imageuri, imageurii;
     private ProgressDialog pDialog;
     String pathtest, pathtest1;
-    //tesing gitinfov
-    //checking pushinfomation..
-
+   
+  
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
